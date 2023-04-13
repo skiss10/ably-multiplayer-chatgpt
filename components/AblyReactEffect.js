7d048acb136c9c5f856c67445784380e2d4533bf
skiss10 <31713441+skiss10@users.noreply.github.com>
2023-04-13 13:01:33 -0700

    added error handling

--- components/AblyReactEffect.js
+++ components/AblyReactEffect.js
@@ -44,19 +44,31 @@ export function useChannel(channelName, callbackOnMessage) {
     return [chatChannel, ably];
 }
 
-// Define the fetchChatGPTResponse function that sends a request to the 
-// OPENAI API and returns the message object with the ChatGPT response data
+// Define the fetchChatGPTResponse function that sends a request to the API
+// and returns the message object with the ChatGPT response data
 async function fetchChatGPTResponse(msg) {
-    // Send a POST request to the API with the message data as the prompt
-    const response = await fetch('/api/chatgpt', {
-        method: 'POST',
-        body: JSON.stringify({ prompt: msg.data }),
-        headers: { 'Content-Type': 'application/json' }
-    });
+    try {
+        // Send a POST request to the API with the message data as the prompt
+        const response = await fetch('/api/chatgpt', {
+            method: 'POST',
+            body: JSON.stringify({ prompt: msg.data }),
+            headers: { 'Content-Type': 'application/json' }
+        });
 
-    // Parse the JSON response
-    const data = await response.json();
+        // Check if the response is ok (status code in the 200-299 range)
+        if (!response.ok) {
+            throw new Error(`API request failed with status ${response.status}`);
+        }
 
-    // Return the message object with the ChatGPT response data
-    return { ...msg, data: `ChatGPT: ${data.response}` };
+        // Parse the JSON response
+        const data = await response.json();
+
+        // Return the message object with the ChatGPT response data
+        return { ...msg, data: `ChatGPT: ${data.response}` };
+    } catch (error) {
+        console.error("Error fetching ChatGPT response:", error);
+
+        // Return the message object with an error message
+        return { ...msg, data: `ChatGPT: Error fetching response - ${error.message}` };
+    }
 }
