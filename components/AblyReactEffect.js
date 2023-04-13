d6d9045fda0afef38ddbe8a00ffc8daa181536a7
skiss10 <31713441+skiss10@users.noreply.github.com>
2023-04-13 12:59:04 -0700

    added comments

--- components/AblyReactEffect.js
+++ components/AblyReactEffect.js
@@ -1,41 +1,62 @@
+// Import the required dependencies
 import Ably from "ably/promises";
 import { useEffect } from 'react'
 
+// Initialize the Ably Realtime instance with the auth URL
 const ably = new Ably.Realtime.Promise({ authUrl: '/api/createTokenRequest' });
 
+// Define the useChannel custom hook
 export function useChannel(channelName, callbackOnMessage) {
+    // Get the chat channel and chatGPT channel from the Ably instance
     const chatChannel = ably.channels.get(channelName);
     const chatGPTChannel = ably.channels.get('chat-gpt');
 
+    // Define the onMount function that will be called when the hook is mounted
     const onMount = () => {
+        // Subscribe to messages from the chat channel and call the provided callback
         chatChannel.subscribe(msg => { callbackOnMessage(msg); });
+
+        // Subscribe to messages from the chatGPT channel, fetch the response,
+        // and call the provided callback with the response
         chatGPTChannel.subscribe(async msg => {
             const response = await fetchChatGPTResponse(msg);
             callbackOnMessage(response);
         });
     }
 
+    // Define the onUnmount function that will be called when the hook is unmounted
     const onUnmount = () => {
+        // Unsubscribe from the chat channel and chatGPT channel
         chatChannel.unsubscribe();
         chatGPTChannel.unsubscribe();
     }
 
+    // Define the useEffectHook that will be passed to the useEffect function
     const useEffectHook = () => {
         onMount();
         return () => { onUnmount(); };
     };
 
+    // Use the useEffect hook with the useEffectHook function
     useEffect(useEffectHook);
 
+    // Return the chat channel and the Ably instance
     return [chatChannel, ably];
 }
 
+// Define the fetchChatGPTResponse function that sends a request to the 
+// OPENAI API and returns the message object with the ChatGPT response data
 async function fetchChatGPTResponse(msg) {
+    // Send a POST request to the API with the message data as the prompt
     const response = await fetch('/api/chatgpt', {
         method: 'POST',
         body: JSON.stringify({ prompt: msg.data }),
         headers: { 'Content-Type': 'application/json' }
     });
+
+    // Parse the JSON response
     const data = await response.json();
+
+    // Return the message object with the ChatGPT response data
     return { ...msg, data: `ChatGPT: ${data.response}` };
 }
