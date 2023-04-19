fc4d59c08bee3f97a0f06278440fc49b53ac580f
skiss10 <31713441+skiss10@users.noreply.github.com>
2023-04-19 10:08:10 -0700

    removed unused function

--- components/AblyReactEffect.js
+++ components/AblyReactEffect.js
@@ -24,31 +24,31 @@ export function useChannel(channelName, callbackOnMessage) {
     return [chatChannel, ably];
   }
 
-// Define the fetchChatGPTResponse function that sends a request to the API
-// and returns the message object with the ChatGPT response data
-async function fetchChatGPTResponse(msg) {
-    try {
-        // Send a POST request to the API with the message data as the prompt
-        const response = await fetch('/api/chatgpt', {
-            method: 'POST',
-            body: JSON.stringify({ prompt: msg.data }),
-            headers: { 'Content-Type': 'application/json' }
-        });
+// // Define the fetchChatGPTResponse function that sends a request to the API
+// // and returns the message object with the ChatGPT response data
+// async function fetchChatGPTResponse(msg) {
+//     try {
+//         // Send a POST request to the API with the message data as the prompt
+//         const response = await fetch('/api/chatgpt', {
+//             method: 'POST',
+//             body: JSON.stringify({ prompt: msg.data }),
+//             headers: { 'Content-Type': 'application/json' }
+//         });
 
-        // Check if the response is ok (status code in the 200-299 range)
-        if (!response.ok) {
-            throw new Error(`API request failed with status ${response.status}`);
-        }
+//         // Check if the response is ok (status code in the 200-299 range)
+//         if (!response.ok) {
+//             throw new Error(`API request failed with status ${response.status}`);
+//         }
 
-        // Parse the JSON response
-        const data = await response.json();
+//         // Parse the JSON response
+//         const data = await response.json();
 
-        // Return the message object with the ChatGPT response data
-        return { ...msg, data: `ChatGPT: ${data.response}` };
-    } catch (error) {
-        console.error("Error fetching ChatGPT response:", error);
+//         // Return the message object with the ChatGPT response data
+//         return { ...msg, data: `ChatGPT: ${data.response}` };
+//     } catch (error) {
+//         console.error("Error fetching ChatGPT response:", error);
 
-        // Return the message object with an error message
-        return { ...msg, data: `ChatGPT: Error fetching response - ${error.message}` };
-    }
-}
+//         // Return the message object with an error message
+//         return { ...msg, data: `ChatGPT: Error fetching response - ${error.message}` };
+//     }
+// }
